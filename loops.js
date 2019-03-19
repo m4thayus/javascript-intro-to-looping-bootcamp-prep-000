96fd86c054fd9199c3d38831be26f5508df5bf98
Matt Williams <matt.seongjun@gmail.com>
2019-03-19 17:42:05 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,3 +1,12 @@
+var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
 function doWhileLoop(n) {
-  
+  do {
+    console.log("I run once regardless");
+  } while (incrementVariable() < n);
 }
