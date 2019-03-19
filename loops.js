853e8475ed5ab5670cfdf5b583ac887d93cb7710
Matt Williams <matt.seongjun@gmail.com>
2019-03-19 17:49:16 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -8,9 +8,9 @@ function incrementVariable() {
 function doWhileLoop(n) {
   do {
     console.log("I run once regardless");
-  } while (incrementVariable() < n) {
-    console.log(i);
-     }
+  } while (incrementVariable() < n);
+  
+  console.log(i);
 }
 
 doWhileLoop(9);
